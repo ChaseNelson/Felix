91ed2efbfa0faf9cabbcde426a2ea2b0149e6b0d
Chase Nelson <chase.a.nelson@gmail.com>
2018-03-15 16:54:30 -0400

    some edit features (#7) change instruct and add img

--- index.js
+++ index.js
@@ -95,12 +95,37 @@ app.get('/fix-it/:machine/:node', (req, res) => {
   res.render('fix', {name:req.params.machine, node:gr[req.params.node], graph:conn})
 });
 
+app.get('/edit', (req, res) => {
+  res.render('editChooseMachine', {ids});
+});
+
+app.get('/edit/:machine', (req, res) => {
+  let g = graphs[req.params.machine];
+  let gr = g['vertices'];
+  let conn = [];
+  for (let i = 0; i < gr[g.rootHash].connected.length; i++) {
+    conn.push(gr[gr[g.rootHash].connected[i]]);
+  }
+  res.render('editMachine', {name:req.params.machine, node:gr[g.rootHash], graph:conn});
+});
+
+app.get('/edit/:machine/:node', (req, res) => {
+  let g = graphs[req.params.machine];
+  let gr = g['vertices'];
+  let conn = [];
+  for (let i = 0; i < gr[req.params.node].connected.length; i++) {
+    conn.push(gr[gr[req.params.node].connected[i]]);
+  }
+  res.render('editMachine', {name:req.params.machine, node:gr[req.params.node], graph:conn})
+});
+
 app.post('/process', (req, res) => {
   if (req.query.form === 'formNewMachine') { /* sent from new-machine form */
     if (typeof graphs[req.body.name] === "undefined") {
       let dir = './public/' + req.body.name;
       if (!fs.existsSync(dir)) {
         fs.mkdirSync(dir);
+        fs.mkdirSync(dir + '/img');
         ids.push(req.body.name);
         graphs[req.body.name] = new Graph(req.body.rootNode);
         console.log(graphs);
@@ -112,6 +137,37 @@ app.post('/process', (req, res) => {
     }
   }
   res.redirect(303, '/save/' + req.body.name);
+});
+
+app.post('/editNode', (req, res) => {
+  let form = new multiparty.Form();
+
+  form.parse(req, (err, fields, files) => {
+    console.log('fields');
+    console.log(fields);
+    let g = graphs[fields.machine[0]];
+    let gr = g['vertices'];
+    let vert = gr[fields.hash[0]];
+
+    // change the instruction
+    vert.instruction = fields.instruction[0];
+
+    // add imgages
+    for (let i = 0; i < files['img'].length; i++) {
+      let file = files['img'][i];
+      if (file['originalFilename'] !== '') {
+        let tmp_path = file['path'];
+        let target_path = './public/' + fields.machine[0] +'/img/' + file['originalFilename'];
+        let dir = './public/' + fields.machine[0] + '/img';
+        if (!fs.existsSync(dir)) {
+          fs.mkdirSync(dir);
+        }
+        fs.renameSync(tmp_path, target_path);
+        g.addImg(fields.hash[0], file['originalFilename']);
+      }
+    }
+    res.redirect(303, '/save/' + fields.machine[0]);
+  });
 });
 
 app.get('/save/:machine', (req, res) => {
